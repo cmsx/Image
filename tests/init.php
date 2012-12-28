9284c02e53c4f629b18141eb9e6593a961e2dbc0
Igor Osharin <igor@osharin.ru>
2012-12-28 10:21:07 +0400

    Структура папок и файлов. Оформлен пакет composer.

---
+++ tests/init.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Файл инициализации для запуска тестов
+ *
+ * Перед запуском сделать composer install
+ * http://getcomposer.org/download/
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/config.php';
---
+++ tests/unit/image.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../init.php';
+
+use CMSx\Image;
+
+class ImageTest extends PHPUnit_Framework_TestCase
+{
+
+}
